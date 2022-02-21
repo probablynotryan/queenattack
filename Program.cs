f6581eb5c15b1dd364bc81d296763128445a583b
Ryan Bass <theryanbass@gmail.com>
2022-02-21 15:17:35 -0800

    add UI prompts and return
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-  
+using System;
+using System.Collections.Generic;
+using Queen.Location;
+
+namespace Program
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter an X coordinate: ");
+      int x = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter a Y coordinate: ");
+      int y = int.Parse(Console.ReadLine());
+      Console.WriteLine(Go.CheckLoc(x, y));
+    }
+  }
+}
